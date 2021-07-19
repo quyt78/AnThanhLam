886bf6f850f1697c4180d1e2ea41cbb590367c75
Can Hoang Quy <canquy@gmail.com>
2021-07-19 18:01:48 +0700

    Filter for product category

---
+++ AnThanhLam.Web/app/shared/filter/statusFilter.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="../assets/admin/libs/bower_components/angular/angular.js" />
+(function (app) {
+    app.filter('statusFilter', function () {
+        return function (input) {
+            if (input == true)
+                return 'Kích hoạt';
+            else
+                return 'Khóa';
+        }
+    })
+})(angular.module('anthanhlam.common'));
